33dc285b5a92f52820ede8ad23849ea463f50c10
Felix Martinez <felixmartinez2503@gmail.com>
2017-05-27 22:33:24 -0400

    Carpeta views

---
+++ GMG/views/templates/head.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title> GMG | cambiar </title>
+  
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <link rel="stylesheet" type="text/css" href= "<?= CSS?>vendor.css">
+  <link rel="stylesheet" type="text/css" href= "<?= CSS?>flat-admin.css">
+
+  <!-- Theme -->
+  <link rel="stylesheet" type="text/css" href= "<?= CSS?>theme/blue-sky.css">
+  <link rel="stylesheet" type="text/css" href= "<?= CSS?>theme/blue.css">
+  <link rel="stylesheet" type="text/css" href= "<?= CSS?>theme/red.css">
+  <link rel="stylesheet" type="text/css" href= "<?= CSS?>theme/yellow.css">
+
+</head>
