4da9d7def29658f81cf7613c022dc0f15d0339fb
krousul <kikemedina.jm@gmail.com>
2017-06-07 05:22:09 -0400

    borrandoooo

--- views/templates/footer.php
+++
@@ -1,9 +0,0 @@
-<footer class="app-footer"> 
-  <div class="row">
-    <div class="col-xs-12">
-      <div class="footer-copyright">
-        Copyright &copy; 2017 Company Fairoz.
-      </div>
-    </div>
-  </div>
-</footer>
